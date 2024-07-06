d5541c2306b04be710449792c8f208f84a9feb6d
BernardTheFour <bernardctxy@gmail.com>
2024-07-06 13:34:33 +0700

    Init file from spring initializr

---
+++ src/main/java/com/betest/gsolusindo/GsolusindoApplication.java
@@ -0,0 +1,13 @@
+package com.betest.gsolusindo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GsolusindoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GsolusindoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/betest/gsolusindo/GsolusindoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.betest.gsolusindo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GsolusindoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
